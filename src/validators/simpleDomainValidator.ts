52c0a51002d2248ae0bd058781cf09d0d6c0bdd3
cptShpek <tarzansh1397@gmail.com>
2025-01-14 16:48:33 +0100

    simple domain validator improvement - no numbers in isp

--- src/validators/simpleDomainValidator.ts
+++ src/validators/simpleDomainValidator.ts
@@ -2,7 +2,11 @@ import { isString } from "../typeGuards/isString";
 
 export const simpleDomainValidator = (value: unknown): boolean => {
   if (isString(value)) {
-    return value.includes(".") && !value.includes("http");
+    const dot = value.includes(".") && !value.includes("http");
+    const end = value.split(".");
+    const endValue = end[end.length - 1].split("");
+
+    return dot && !endValue.some((v) => typeof +v === "number");
   }
   return false;
 };
